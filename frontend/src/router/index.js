b728456dcc057404197251e982ce7904de968b4a
jass-coder <jramgarhia530@gmail.com>
2026-05-28 08:00:54 +0530

    made changes on who we are section

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,24 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash,
+        top: 88,
+        behavior: 'smooth',
+      }
+    }
+
+    return {
+      top: 0,
+      behavior: 'smooth',
+    }
+  },
 })
 
 export default router
